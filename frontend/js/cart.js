5e9c7294ee9d37b22864e99f44ea80a7206d01f6
Avnish-coder <avnishsharma0420@gmail.com>
2022-04-22 09:43:57 +0530

    FinalMongoose

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -1,62 +1,103 @@
 let tbody = document.querySelector("tbody");
-let Ids = JSON.parse(localStorage.getItem("productIds"));
+// let Ids = getCartArray();
+let Ids;
 let checkBox = document.querySelector(`input[type = "checkbox"]`);
 let shipping = document.querySelector(`.shipping`);
 let totals = document.querySelectorAll(".new__price.Subtotal");
 
 let products;
 
-
-
 window.addEventListener("DOMContentLoaded", async function () {
-              products = await getProducts();
-              displayCart(products);
-              deleteCart();
-                addDIP();
-              cartTotal.innerText = localStorage.getItem("Items");
-             
+  Ids = await getCartArray();
+  products = await getProducts();
+
+  // const results = await fetch("/getCart");
+  // const data = await results.json();
+  // const items = data.carts[0].items;
+  // console.log(items);
+  // cartTotal.innerText = localStorage.getItem("Items");
+  // cartTotal.innerText = items
+  await getCartItem();
+  await displayCart(products);
+  deleteCart();
+  addDIP();
 });
 
-function deleteCart() {
-              let trashes = document.querySelectorAll(".trashCane");
-              
-              trashes.forEach(trash => {
-                            // console.log(sibling);
-                            trash.addEventListener('click', (e) => {
-                                        //   let sibling = parseInt("-" + trash.previousElementSibling.innerText.slice(1));
-                                        //   console.log(sibling);
-
-                                          let nIds = Ids.filter((val) => val != e.target.id);
-                                        localStorage.setItem("productIds", JSON.stringify(nIds));
-                                        //   subT(sibling);
-                                           localStorage.setItem("Items",nIds.length );
-                                          displayCart();
-                                        //   console.log("1");
-                                        //   location.reload();
-                                        deleteCart();
-                                        addDIP();
-                                        // localStorage.setItem("Items",nIds.length );
-                                        cartTotal.innerText = localStorage.getItem("Items");
-                                        
-                                          
-                            })
-              })
-
-
+async function getCartItem() {
+  const results = await fetch("/getCart");
+  const data = await results.json();
+  const items = data.carts[0].items;
+  console.log(items);
+  // cartTotal.innerText = localStorage.getItem("Items");
+  cartTotal.innerText = items;
 }
 
-function displayCart(items) {
-              Ids = JSON.parse(localStorage.getItem("productIds"));
-              let idsSet = new Set(Ids);
+async function setCartItems(id, items) {
+  const results = await fetch("/postCart", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ items: items, productIds: id }),
+  });
 
-              let filterArr = products.filter((item) => {
-                                return idsSet.has(item.id.toString());
-              });
+  // cartTotal.innerText = localStorage.getItem("Items");
+  // cartTotal.innerText = items
+}
 
-              tbody.innerHTML = "";
+function deleteCart() {
+  let trashes = document.querySelectorAll(".trashCane");
+  trashes.forEach((trash) => {
+    // console.log(sibling);
+    trash.addEventListener("click", async (e) => {
+      //   let sibling = parseInt("-" + trash.previousElementSibling.innerText.slice(1));
+      //   console.log(sibling);
 
-              filterArr.forEach(product => {
-                            tbody.innerHTML += `<tr>
+      let nIds = Ids.filter((val) => val != e.target.id);
+      console.log(nIds);
+      console.log(nIds.length);
+      console.log("delete Cart");
+
+      //   localStorage.setItem("productIds", JSON.stringify(nIds));
+      //   //   subT(sibling);
+      //   localStorage.setItem("Items", nIds.length);
+      await setCartItems(nIds, nIds.length);
+      await displayCart();
+      //   console.log("1");
+      //   location.reload();
+      deleteCart();
+      addDIP();
+      // localStorage.setItem("Items",nIds.length );
+      //   cartTotal.innerText = localStorage.getItem("Items");
+      await getCartItem();
+    });
+  });
+}
+
+async function getCartArray() {
+  const results = await fetch("/getCart");
+  const data = await results.json();
+  const items = data.carts[0].productIds;
+//   console.log(items);
+  // cartTotal.innerText = localStorage.getItem("Items");
+  return items;
+}
+
+async function displayCart(items) {
+  Ids = await getCartArray();
+
+  //   Ids = JSON.parse(localStorage.getItem("productIds"));
+  let idsSet = new Set(Ids);
+
+  let filterArr = products.filter((item) => {
+    return idsSet.has(item.id.toString());
+  });
+
+  tbody.innerHTML = "";
+
+  filterArr.forEach((product) => {
+    tbody.innerHTML += `<tr>
                                 <td class="product__thumbnail">
                                     <a href="#">
                                         <img src="${product.image}" alt="">
@@ -87,75 +128,63 @@ function displayCart(items) {
                                     </div>
                                     <i class="fa fa-trash remove__cart-item trashCane" id="${product.id}" style = "font-size : 20px" aria-hidden="false"></i>
                                 </td>
-                                </tr>`}
-              );
+                                </tr>`;
+  });
 
-            //   deleteCart();
-            //   addDIP();
+  //   deleteCart();
+  //   addDIP();
 }
 
-
-
 function addDIP() {
-              const minus = document.querySelectorAll(".minus-btn");
-              const plus = document.querySelectorAll(".plus-btn");
-              const input = document.querySelectorAll(".counter-btn");
-              const aPrice = document.querySelectorAll(".aPrice");
-              
+  const minus = document.querySelectorAll(".minus-btn");
+  const plus = document.querySelectorAll(".plus-btn");
+  const input = document.querySelectorAll(".counter-btn");
+  const aPrice = document.querySelectorAll(".aPrice");
 
-              for (let i = 0; i < minus.length; i++) {
-                            let currPrice = minus[i].getAttribute("data-price")
-                            minus[i].addEventListener("click", () => {
+  for (let i = 0; i < minus.length; i++) {
+    let currPrice = minus[i].getAttribute("data-price");
+    minus[i].addEventListener("click", () => {
+      if (input[i].value > 1) {
+        input[i].value--;
+        aPrice[i].innerText = "$" + input[i].value * parseInt(currPrice);
+        subT(getPrice());
+        // subT(parseInt(aPrice[i].innerText.slice(1)) - parseInt(currPrice) - parseInt(currPrice));
+      }
+    });
 
-                                          if (input[i].value > 1) {
-                                                        input[i].value--;
-                                                        aPrice[i].innerText = "$" + input[i].value * parseInt(currPrice);
-                                                        subT(getPrice());
-                                                        // subT(parseInt(aPrice[i].innerText.slice(1)) - parseInt(currPrice) - parseInt(currPrice));
-                                          }
-                            });
+    plus[i].addEventListener("click", () => {
+      input[i].value++;
+      aPrice[i].innerText = "$" + input[i].value * parseInt(currPrice);
+      //   subT(parseInt(aPrice[i].innerText.slice(1)) - parseInt(currPrice));
+      subT(getPrice());
+    });
+  }
 
-                            plus[i].addEventListener("click", () => {
+  function getPrice() {
+    let param = 0;
+    for (let i = 0; i < aPrice.length; i++) {
+      param += parseInt(aPrice[i].innerText.slice(1));
+    //   console.log(param);
+    }
+    // subT(param);
+    return param;
+  }
 
-                                          input[i].value++;
-                                          aPrice[i].innerText = "$" + input[i].value * parseInt(currPrice);
-                                        //   subT(parseInt(aPrice[i].innerText.slice(1)) - parseInt(currPrice));
-                                        subT(getPrice());
-                            });
-              }
-              function getPrice() {
-                let param = 0;
-                for (let i = 0; i < aPrice.length; i++) {
-                    param += parseInt(aPrice[i].innerText.slice(1));
-                    console.log(param);
-                    }
-                    // subT(param);
-                  return param;
-              }
-
-              
-                subT(getPrice());
-
-
+  subT(getPrice());
 }
 
 function subT(price) {
-              totals[0].innerText = price;
-              totals[1].innerText = price;
-              
+  totals[0].innerText = price;
+  totals[1].innerText = price;
+}
 
-};
-
-
-
-checkBox.addEventListener('change', function () {
-    let totalprice = parseInt(totals[0].innerText);
-              if (this.checked) {
-                            totalprice = "$7";
-                            subT(totalprice + 7);
-
-              } else {
-                            shipping.innerText = "$0";
-                            subT(totalprice - 7);
-              }
+checkBox.addEventListener("change", function () {
+  let totalprice = parseInt(totals[0].innerText);
+  if (this.checked) {
+    totalprice = "$7";
+    subT(totalprice + 7);
+  } else {
+    shipping.innerText = "$0";
+    subT(totalprice - 7);
+  }
 });
--- frontend/js/products.js
+++ frontend/js/products.js
@@ -1,12 +1,12 @@
-const getProducts = async () => {
-  try {
-    const results = await fetch("./data/products.json");
-    const data = await results.json();
-    const products = data.products;
-    return products;
-  } catch (err) {
-    console.log(err);
-  }
+const getProducts = async() => {
+    try {
+        const results = await fetch("/products");
+        const data = await results.json();
+        const products = data.products;
+        return products;
+    } catch (err) {
+        console.log(err);
+    }
 };
 /*
 =============
@@ -17,16 +17,16 @@ Load Category Products
 let cartTotal = document.querySelector("#cart__total");
 const categoryCenter = document.querySelector(".category__center");
 
-window.addEventListener("DOMContentLoaded", async function () {
-  const products = await getProducts();
-  displayProductItems(products);
-  addCart();
-  cartTotal.innerText = localStorage.getItem("Items");
+window.addEventListener("DOMContentLoaded", async function() {
+    const products = await getProducts();
+    displayProductItems(products);
+    addCart();
+    cartTotal.innerText = await getCartItem1();
 });
 
-const displayProductItems = items => {
-  let displayProduct = items.map(
-    product => ` 
+const displayProductItems = (items) => {
+    let displayProduct = items.map(
+        (product) => ` 
                   <div class="product category__products">
                     <div class="product__header">
                       <img src=${product.image} alt="product">
@@ -80,16 +80,13 @@ const displayProductItems = items => {
                   </ul>
                   </div>
                   `
-  );
+    );
 
-  displayProduct = displayProduct.join("");
-  if (categoryCenter) {
-    categoryCenter.innerHTML = displayProduct;
-
-  }
+    displayProduct = displayProduct.join("");
+    if (categoryCenter) {
+        categoryCenter.innerHTML = displayProduct;
+    }
 };
-
-
 
 /*
 =============
@@ -101,36 +98,36 @@ const filterBtn = document.querySelectorAll(".filter-btn");
 const categoryContainer = document.getElementById("category");
 
 if (categoryContainer) {
-  categoryContainer.addEventListener("click", async e => {
-    const target = e.target.closest(".section__title");
-    if (!target) return;
+    categoryContainer.addEventListener("click", async(e) => {
+        const target = e.target.closest(".section__title");
+        if (!target) return;
 
-    const id = target.dataset.id;
-    const products = await getProducts();
+        const id = target.dataset.id;
+        const products = await getProducts();
 
-    if (id) {
-      // remove active from buttons
-      Array.from(filterBtn).forEach(btn => {
-        btn.classList.remove("active");
-      });
-      target.classList.add("active");
+        if (id) {
+            // remove active from buttons
+            Array.from(filterBtn).forEach((btn) => {
+                btn.classList.remove("active");
+            });
+            target.classList.add("active");
 
-      // Load Products
-      let menuCategory = products.filter(product => {
-        if (product.category === id) {
-          return product;
+            // Load Products
+            let menuCategory = products.filter((product) => {
+                if (product.category === id) {
+                    return product;
+                }
+            });
+
+            if (id === "All Products") {
+                displayProductItems(products);
+                addCart();
+            } else {
+                displayProductItems(menuCategory);
+                addCart();
+            }
         }
-      });
-
-      if (id === "All Products") {
-        displayProductItems(products);
-        addCart();
-      } else {
-        displayProductItems(menuCategory);
-        addCart();
-      }
-    }
-  });
+    });
 }
 
 /*
@@ -153,29 +150,29 @@ const picList = [pic1, pic2, pic3, pic4, pic5];
 // Active Picture
 let picActive = 1;
 
-["mouseover", "touchstart"].forEach(event => {
-  if (picContainer) {
-    picContainer.addEventListener(event, e => {
-      const target = e.target.closest("img");
-      if (!target) return;
-      const id = target.id.slice(3);
-      changeImage(`./images/products/iPhone/iphone${id}.jpeg`, id);
-    });
-  }
+["mouseover", "touchstart"].forEach((event) => {
+    if (picContainer) {
+        picContainer.addEventListener(event, (e) => {
+            const target = e.target.closest("img");
+            if (!target) return;
+            const id = target.id.slice(3);
+            changeImage(`./images/products/iPhone/iphone${id}.jpeg`, id);
+        });
+    }
 });
 
 // change active image
 const changeImage = (imgSrc, n) => {
-  // change the main image
-  pic.src = imgSrc;
-  // change the background-image
-  zoom.style.backgroundImage = `url(${imgSrc})`;
-  //   remove the border from the previous active side image
-  picList[picActive - 1].classList.remove("img-active");
-  // add to the active image
-  picList[n - 1].classList.add("img-active");
-  //   update the active side picture
-  picActive = n;
+    // change the main image
+    pic.src = imgSrc;
+    // change the background-image
+    zoom.style.backgroundImage = `url(${imgSrc})`;
+    //   remove the border from the previous active side image
+    picList[picActive - 1].classList.remove("img-active");
+    // add to the active image
+    picList[n - 1].classList.add("img-active");
+    //   update the active side picture
+    picActive = n;
 };
 
 /*
@@ -189,29 +186,28 @@ const detail = document.querySelector(".product-detail__bottom");
 const contents = document.querySelectorAll(".content");
 
 if (detail) {
-  detail.addEventListener("click", e => {
-    const target = e.target.closest(".detail-btn");
-    if (!target) return;
+    detail.addEventListener("click", (e) => {
+        const target = e.target.closest(".detail-btn");
+        if (!target) return;
 
-    const id = target.dataset.id;
-    if (id) {
-      Array.from(btns).forEach(btn => {
-        // remove active from all btn
-        btn.classList.remove("active");
-        e.target.closest(".detail-btn").classList.add("active");
-      });
-      // hide other active
-      Array.from(contents).forEach(content => {
-        content.classList.remove("active");
-      });
-      const element = document.getElementById(id);
-      element.classList.add("active");
-    }
-  });
+        const id = target.dataset.id;
+        if (id) {
+            Array.from(btns).forEach((btn) => {
+                // remove active from all btn
+                btn.classList.remove("active");
+                e.target.closest(".detail-btn").classList.add("active");
+            });
+            // hide other active
+            Array.from(contents).forEach((content) => {
+                content.classList.remove("active");
+            });
+            const element = document.getElementById(id);
+            element.classList.add("active");
+        }
+    });
 }
 
 // shop now button
-
 
 // searching by typing starts
 
@@ -219,145 +215,157 @@ const searchForm = document.querySelector("#search-form");
 // console.log(searchForm);
 const searchFormInput = searchForm.querySelector("input"); // <=> document.querySelector("#search-form input");
 
-searchFormInput.addEventListener("keypress", async function (e) {
-
+searchFormInput.addEventListener("keypress", async function(e) {
     searchProduct(e.target.value);
-  
 });
 
-
 async function searchProduct(input) {
-  let productSearching = await getProducts();
+    let productSearching = await getProducts();
     // console.log(productSearching);
-    if(input == ""){
-      const products = await getProducts();
-      displayProductItems(products);
-      addCart();
-      return;
+    if (input == "") {
+        const products = await getProducts();
+        displayProductItems(products);
+        addCart();
+        return;
     }
 
-    let products = productSearching.filter(function (prd) {
-     
-      return prd.title.toLowerCase() == input.toLowerCase();
-      
+    let products = productSearching.filter(function(prd) {
+        return prd.title.toLowerCase() == input.toLowerCase();
     });
     document.getElementById("category").scrollIntoView();
-      displayProductItems(products);
-      addCart();
+    displayProductItems(products);
+    addCart();
 }
- 
 
 // The speech recognition interface lives on the browser’s window object
 
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; // if none exists -> undefined
+const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition; // if none exists -> undefined
 
-if(SpeechRecognition) {
-  console.log("Your Browser supports speech Recognition");
-  
-  const recognition = new SpeechRecognition();
-  recognition.continuous = true;
-  // recognition.lang = "en-US";
+if (SpeechRecognition) {
+    console.log("Your Browser supports speech Recognition");
 
-  searchForm.insertAdjacentHTML("beforeend", ' <button type="button" class="icon__item icon__cart" ><i class="fa fa-microphone" aria-hidden="true" style = "width : 17px" ></i></i></button>' );
-  searchFormInput.style.paddingRight = "50px";
+    const recognition = new SpeechRecognition();
+    recognition.continuous = true;
+    // recognition.lang = "en-US";
 
-  const micBtn = searchForm.querySelector("button");
-  const micIcon = micBtn.firstElementChild;
+    searchForm.insertAdjacentHTML(
+        "beforeend",
+        ' <button type="button" class="icon__item icon__cart" ><i class="fa fa-microphone" aria-hidden="true" style = "width : 17px" ></i></i></button>'
+    );
+    searchFormInput.style.paddingRight = "50px";
 
-  micBtn.addEventListener("click", micBtnClick);
-  function micBtnClick() {
-    if(micIcon.classList.contains("fa-microphone")) { // Start Voice Recognition
-      recognition.start(); // First time you have to allow access to mic!
+    const micBtn = searchForm.querySelector("button");
+    const micIcon = micBtn.firstElementChild;
+
+    micBtn.addEventListener("click", micBtnClick);
+
+    function micBtnClick() {
+        if (micIcon.classList.contains("fa-microphone")) {
+            // Start Voice Recognition
+            recognition.start(); // First time you have to allow access to mic!
+        } else {
+            recognition.stop();
+        }
     }
-    else {
-      recognition.stop();
+
+    recognition.addEventListener("start", startSpeechRecognition); // <=> recognition.onstart = function() {...}
+    function startSpeechRecognition() {
+        micIcon.classList.remove("fa-microphone");
+        micIcon.classList.add("fa-microphone-slash");
+        searchFormInput.focus();
+        console.log("Voice activated, SPEAK");
     }
-  }
 
-  recognition.addEventListener("start", startSpeechRecognition); // <=> recognition.onstart = function() {...}
-  function startSpeechRecognition() {
-    micIcon.classList.remove("fa-microphone");
-    micIcon.classList.add("fa-microphone-slash");
-    searchFormInput.focus();
-    console.log("Voice activated, SPEAK");
-  }
+    recognition.addEventListener("end", endSpeechRecognition); // <=> recognition.onend = function() {...}
+    function endSpeechRecognition() {
+        micIcon.classList.remove("fa-microphone-slash");
+        micIcon.classList.add("fa-microphone");
+        searchFormInput.focus();
+        console.log("Speech recognition service disconnected");
+    }
 
-  recognition.addEventListener("end", endSpeechRecognition); // <=> recognition.onend = function() {...}
-  function endSpeechRecognition() {
-    micIcon.classList.remove("fa-microphone-slash");
-    micIcon.classList.add("fa-microphone");
-    searchFormInput.focus();
-    console.log("Speech recognition service disconnected");
-  }
+    recognition.addEventListener("result", resultOfSpeechRecognition); // <=> recognition.onresult = function(event) {...} - Fires when you stop talking
+    function resultOfSpeechRecognition(event) {
+        const current = event.resultIndex;
+        const transcript = event.results[current][0].transcript;
 
-  recognition.addEventListener("result", resultOfSpeechRecognition); // <=> recognition.onresult = function(event) {...} - Fires when you stop talking
-  function resultOfSpeechRecognition(event) {
-    const current = event.resultIndex;
-    const transcript = event.results[current][0].transcript;
+        searchFormInput.value = transcript;
+        // if(transcript != ""){
+        //   searchProduct(transcript);
+        // }else{
+        //   await getProducts();
+        // }
+        searchProduct(transcript);
 
-    searchFormInput.value = transcript;
-    // if(transcript != ""){
-    //   searchProduct(transcript);
-    // }else{
-    //   await getProducts();
-    // }
-    searchProduct(transcript);
-    
-      
-    console.log(transcript);
-  }
-  
-  
+        console.log(transcript);
+    }
+} else {
+    console.log("Your Browser does not support speech Recognition");
 }
-else {
-  console.log("Your Browser does not support speech Recognition");
+
+
+
+
+
+async function getCartItem1 () {
+    const results = await fetch("/getCart");
+    const data = await results.json();
+    const items = data.carts[0].items;
+    // console.log(items);
+    // cartTotal.innerText = localStorage.getItem("Items");
+    return items
+}
+
+ async function setCartItems1(id,items) {
+     console.log(id);
+     console.log(items);
+    const results = await fetch("/postCart",{
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({items: id, productIds : items})
+      });
+      
+    // cartTotal.innerText = localStorage.getItem("Items");
+    // cartTotal.innerText = items
+
 }
 
 // searching by typing end
 
-
 // increase plus minus
 
-// adding functionality 
-
+// adding functionality
 
 // rendering products
 let productIds = new Set();
 
-
-
 function addCart() {
+    let cartButton = document.querySelectorAll(".product__btn");
 
-let cartButton = document.querySelectorAll(".product__btn");
+    // console.log(tbody);
 
+    for (let i = 0; i < cartButton.length; i++) {
+        cartButton[i].addEventListener("click", async function(e) {
+            // let getItem = localStorage.getItem("Items") || 0;
+            let getItem = await getCartItem1();
+            getItem = parseInt(getItem);
+            cartTotal.innerText = getItem + 1;
 
-// console.log(tbody);
+            productIds.add(e.target.id);
+            // console.log(productIds);
+            let eArr = [];
+            productIds.forEach((val) => {
+                eArr.push(val);
+            });
 
-for(let i = 0 ; i <  cartButton.length; i++){
-
-  cartButton[i].addEventListener("click",function (e) {
-    let getItem = localStorage.getItem("Items") || 0;
-    getItem = parseInt(getItem);
-    cartTotal.innerText = getItem + 1;
-    
-    productIds.add(e.target.id);
-    // console.log(productIds);
-    let eArr = [];
-    productIds.forEach((val)=>{
-      eArr.push(val);
-
-    });
-
-    // console.log(JSON.stringify(productIds));
-    localStorage.setItem("productIds",JSON.stringify(eArr));
-    localStorage.setItem("Items",getItem + 1);
-
-  });
-  
+            // console.log(JSON.stringify(productIds));
+            // localStorage.setItem("productIds", JSON.stringify(eArr));
+            // localStorage.setItem("Items", getItem + 1);
+            await setCartItems1(getItem + 1,eArr);
+        });
+    }
 }
-  
-}
-
-
-
-
